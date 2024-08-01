5d8cae9645192fafff0b03589777c98aa76c4944
Colby Jensen <endergdawg@gmail.com>
2024-08-01 08:45:57 -0600

    add semicolons

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,15 @@
 function fizzbuzz(){
-  count = 100
-  fizz = "fizz"
-  buzz = "buzz"
+  const count = 100;
+  const fizz = "fizz";
+  const buzz = "buzz";
   for (let i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0){
-      console.log(fizz + buzz + i)
+      console.log(fizz + buzz + i);
     }else if (i % 3 == 0){
-      console.log(fizz + i)
+      console.log(fizz + i);
     }else if (i % 5 == 0){
-      console.log(buzz + i)
+      console.log(buzz + i);
     }
   }
 }
-fizzbuzz()
+fizzbuzz();
